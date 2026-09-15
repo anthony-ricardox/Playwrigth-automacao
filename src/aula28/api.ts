cf893443f5b617163a64c8da5d36a645a2f8292b
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-09-15 14:09:30 -0300

    consumindo api /Get

--- src/aula28/api.ts
+++ src/aula28/api.ts
@@ -1,87 +1,57 @@
 
-
-type Post ={
-  id?: number;
-  userId:number;
-  title: string;
-  body: string;
-}
-
-type Comment = {
-  postId: number;
-  id: number;
-  name: string;
-  email: string;
-  body: string;
-}
-
 const Api:string =   'https://jsonplaceholder.typicode.com'
 const Lista:string = 'https://jsonplaceholder.typicode.com/todos' 
 const Buscar:string =`https://jsonplaceholder.typicode.com/todos/1`
-const Post:string =  'https://jsonplaceholder.typicode.com/posts'
+const post:string =  'https://jsonplaceholder.typicode.com/posts'
 const Update:string =`https://jsonplaceholder.typicode.com/posts/1`
 
-const Enviar :Post = ( title, body) => {
 
-}
-async function ListarPost() {
-  const res = await fetch(` ${Post}`)
-  const dados:Post[] = await res.json();
+//Definindo contratos de tipos
+type Post = {
+    userId:number;
+    id?: number; //campo opcional
+    title: string;
+    body: string;
+};
+type Coment ={
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
+};
 
-  console.log(`Status: ${res.status}`)
-  console.log(` Lidos: ${dados.length} posts` )
-  console.log(`Titulo: ${dados[0].title}`)
-}
-
-ListarPost()
-
-/*async function ListarDados() {
-  const r = await fetch(`${Lista}`)
-  const dados:Post[] = await r.json();
-
-  console.log(`Title: ${dados}, Body: ${dados}`)
-} 
-
-ListarDados()*/
-
-// Get /post/1
-async function BuscarId(id:number) {
-  console.log('--- 2. GET /posts/1')
-  const res = await fetch(`${Buscar}/${id}`)
-  const dados: Post[] = await res.jason();
-
-  console.log(`Status: ${res.status}`)
-  console.log(`Titulo: ${dados[id].title}`)
+//GET /posts
+async function listarPost(){
+    console.log(`--- 1. GET /posts ---`);
+    const res = await fetch(`${Api}/posts`);
+    const dados: Post[] = await res.json();
+    console.log(`✅ Status: ${res.status}`);
+    console.log(`Lidos ${dados.length} posts. Ex: do primeiro:`,dados[0].title);
 }
 
+// GET /posts/1
 
-
-// Get /posts/1/comment
-async function  ListarComment(postId:number) {
-  console.log('--- 3. GET /posts/1/comment')
-  const res = await fetch(`${Buscar}/posts/${postId}/comments`)
-  const dados: Comment[] = await res.jason();
-
-  console.log(`Status: ${res.status}`)
-  console.log(`O post ${postId} tem ${dados.length} comment
-            EX: Email do primeiro comentario. `, dados[0].email)
+async function buscarPorId(id:number) {
+    console.log(`--- 2. GET /posts/1 ---`);
+    const res = await fetch(`${Api}/posts/${id}`);
+    const dados: Post = await res.json();
+    console.log(`✅ Status: ${res.status}`);
+    console.log(`Título do post ${id}:`, dados.title);
 }
 
-
-
-/*async function criaPost(title: string): Promises<Post> {
-  const res = await fetch("https://jsonplaceholder/posts", {
-    method: "POST",
-    headers: { "Content-Type": "aplication/json" },
-    body: JSON.stringify({ title: titulo, body: userId }),
-  });
-
-  if(res.ok){
-    try {
-        
-    } catch (error) {
-        
-    }
-  }
+// GET /post/1/comments
+async function listarComent(postId:number){
+    console.log(`--- 3. GET /posts/1/comments ---`);
+    const res = await fetch(`${Api}/posts/${postId}/comments`);
+    const dados: Coment[] = await res.json();
+    console.log(`✅ Status: ${res.status}`);
+    console.log(`O post ${postId} tem ${dados.length} comentários Ex: Email do primeiro comentário.`, dados[0].email);
 }
-*/
+async function chamarReqs(){
+    listarPost();
+    buscarPorId(10);
+    listarComent(20);
+
+}
+chamarReqs();
